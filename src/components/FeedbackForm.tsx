59a329521a476d8c4f9d25739340769ee9be82e3
Eric Tran <erictran1547@gmail.com>
2024-07-17 09:45:13 -0700

    Intial setup of the project structure and CSS files

---
+++ src/components/FeedbackForm.tsx
@@ -0,0 +1,21 @@
+export default function FeedbackForm() {
+  return (
+    <form className="form">
+      <textarea
+        name=""
+        id="feedback-textarea"
+        placeholder="blabla"
+        spellCheck={false}
+      ></textarea>
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+      <div>
+        <p className="u-italic">150</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://google.com">
+            CorpComment.com
+          </a>
+          . Intended for learning or portfolio
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching material
+        </p>
+      </small>
+    </footer>
+  );
+}
